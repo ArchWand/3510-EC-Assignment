ed8cddb3ce781a9bd535c52d210c258265d8cc81
Robert Zhu <arcwandx86@gmail.com>
2024-11-11 23:36:30 -0500

    Initial commit

---
+++ Solutions.py
@@ -0,0 +1,105 @@
+# DO NOT ADD ANY IMPORTS
+from typing import *
+
+class Solutions:
+    # DO NOT MODIFY INIT
+    def __init__(self):
+        pass
+
+    # Problem 1: Real Estate Profits
+    # Explanation and Runtime:
+    #
+    #
+    def realEstatePrices(values: List[int]) -> int:
+        return -1
+
+    # Problem 2: Warehouse Package Stacking
+    # Explanation and Runtime:
+    #
+    #
+    def maxPackages(packages: List[Tuple[int, int]]) -> int:
+        return -1
+    
+    # Problem 3: Building Blocks
+    # Explanation and Runtime:
+    #
+    #
+    def minSwap(nums1: List[int], nums2: List[int]) -> int:
+        return -1
+    
+    # Problem 4: Modular Two Sum 
+    # Explanation and Runtime:
+    #
+    #
+    def modTwoSum(A: List[int], k: int) -> int:
+        return -1
+
+    # Problem 5: Maximum Magic Path Power
+    # Explanation and Runtime:
+    #
+    #
+    def maximumMagicPathPower(energies: List[int], edges: List[List[int]], maxTime: int) -> int:
+        return -1
+    
+    # Problem 6: Divide the Harvest
+    # Explanation and Runtime:
+    #
+    #
+    def divideTheHarvest(quantity: List[int], k: int) -> int:
+        return -1
+      
+    # Problem 7: Coloring Sidewalks 
+    # Explanation and Runtime:
+    #
+    #
+    def coloringSidewalks(time: List[int]) -> int:
+        return -1
+    
+    # Problem 8: Chemical Concoctions
+    # Explanation and Runtime:
+    #
+    #
+    def chemicalConcoctions(formulas: List[str]) -> str:
+        return ""
+    
+    # Problem 9: Maximum Sum of Non-Adjacent Subsequences
+    # Explanation and Runtime:
+    #
+    #
+    def maxNonAdjSum(A: List[int]) -> int:
+        return -1
+    
+    # Problem 10: DigitGPT
+    # Explanation and Runtime:
+    #
+    #
+    def reviveStrings(n: int, ticket: str) -> bool:
+        return False
+    
+    # Problem 11: Building a Brick Wall
+    # Explanation and Runtime:
+    #
+    #
+    def buildBrickWall(B: List[int], l: int) -> int:
+        return -1
+
+    # Problem 12: Archipelagos
+    # Explanation and Runtime:
+    #
+    #
+    def findNeededBridges(n: int, edges: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+        return []
+    
+    # Problem 13: Search Engineer 
+    # Explanation and Runtime:
+    #
+    #
+    def numDistinct(text: str, pattern: str) -> int:
+        return -1
+
+    # Problem 14: Buzz's Bees
+    # Explanation and Runtime:
+    #
+    #
+    def minNetworkCost(coords: list[tuple[int, int]]) -> int:
+        return -1
