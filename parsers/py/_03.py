e5c48591557200e3697bb298c3865f0f79c2d691
Robert Zhu <arcwandx86@gmail.com>
2024-12-01 17:38:23 -0500

    Use spaces instead of tabs

--- parsers/py/_03.py
+++ parsers/py/_03.py
@@ -1,26 +1,26 @@
 def parser(f):
-	n = int(f.readline())
-	blocks1 = [ int(f.readline()) for _ in range(n) ]
-	blocks2 = [ int(f.readline()) for _ in range(n) ]
+    n = int(f.readline())
+    blocks1 = [ int(f.readline()) for _ in range(n) ]
+    blocks2 = [ int(f.readline()) for _ in range(n) ]
 
-	assert(f.readline() == "\n")
+    assert(f.readline() == "\n")
 
-	cert = int(f.readline())
-	assert(f.readline().strip() == "")
+    cert = int(f.readline())
+    assert(f.readline().strip() == "")
 
-	return cert, (blocks1, blocks2,)
+    return cert, (blocks1, blocks2,)
 
 def verifier(cert, ans):
-	return cert == ans
+    return cert == ans
 
 def error(cert, input, ans):
-	print("Input:")
-	print("blocks1 = " + str(input[0]))
-	print("blocks2 = " + str(input[1]))
-	print("")
-	print("Expected:")
-	print(cert)
-	print("")
-	print("Actual:")
-	print(ans)
-	print()
+    print("Input:")
+    print("blocks1 = " + str(input[0]))
+    print("blocks2 = " + str(input[1]))
+    print("")
+    print("Expected:")
+    print(cert)
+    print("")
+    print("Actual:")
+    print(ans)
+    print()
--- parsers/py/_13.py
+++ parsers/py/_13.py
@@ -1,25 +1,25 @@
 def parser(f):
-	text = f.readline().strip()
-	pattern = f.readline().strip()
+    text = f.readline().strip()
+    pattern = f.readline().strip()
 
-	assert(f.readline() == "\n")
+    assert(f.readline() == "\n")
 
-	cert = int(f.readline())
-	assert(f.readline().strip() == "")
+    cert = int(f.readline())
+    assert(f.readline().strip() == "")
 
-	return cert, (text, pattern,)
+    return cert, (text, pattern,)
 
 def verifier(cert, ans):
-	return cert == ans
+    return cert == ans
 
 def error(cert, input, ans):
-	print("Input:")
-	print("text = " + str(input[0]))
-	print("pattern = " + str(input[1]))
-	print("")
-	print("Expected:")
-	print(cert)
-	print("")
-	print("Actual:")
-	print(ans)
-	print()
+    print("Input:")
+    print("text = " + str(input[0]))
+    print("pattern = " + str(input[1]))
+    print("")
+    print("Expected:")
+    print(cert)
+    print("")
+    print("Actual:")
+    print(ans)
+    print()
